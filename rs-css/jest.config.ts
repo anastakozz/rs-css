4e3fdfd985292c4aafa4adbc53cbbca3400dfee9
anastakozz <117598324+anastakozz@users.noreply.github.com>
2023-07-05 23:11:58 +0300

    fix: edit jest.config

--- rs-css/jest.config.ts
+++ rs-css/jest.config.ts
@@ -2,9 +2,7 @@ import type {Config} from 'jest';
 
 const config: Config = {
   preset: 'ts-jest',
-  collectCoverage: true,
-  coverageDirectory: "coverage",
-  coverageProvider: "v8",
+  testEnvironment: 'node',
   transform: {
     '^.+\\.ts?$': 'ts-jest',
   },
