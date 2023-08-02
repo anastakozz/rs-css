1871e38f3050e3ee6c0201d1c5ddd7d4f29d8b83
anastakozz <117598324+anastakozz@users.noreply.github.com>
2023-08-02 14:16:20 +0300

    fix: enable strict comparison

--- rs-css/webpack.config.js
+++ rs-css/webpack.config.js
@@ -4,7 +4,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const EslingPlugin = require('eslint-webpack-plugin');
 
-const isProduction = process.env.NODE_ENV == "production";
+const isProduction = process.env.NODE_ENV === "production";
 
 const stylesHandler = "style-loader";
 
